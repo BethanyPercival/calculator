8d7a7784eaadaf7ce95466af0e1555a50844eaaa
bethanypercival <bethany.percival@ukfast.co.uk>
2018-04-17 08:41:12 +0100

    First commit with layout working

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -8,18 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+enum EquationType: Int {
+    case multiply, subtract, add, equals, none
 }
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var verticalStackView: UIStackView!
+    @IBOutlet weak var display: UILabel!
+    var equationType: EquationType = .none
+    
+    var btnStrings: [[String]] {
+        return [
+            ["7", "8", "9", "x"],
+            ["4", "5", "6", "-"],
+            ["1", "2", "3", "+"],
+            ["0", "AC", ".", "="]
+        ]
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        
+    }
+    
+    func setup() {
+        guard let stackArr: [UIStackView] = verticalStackView.arrangedSubviews as? [UIStackView] else {
+            return
+        }
+        
+        for (verIdx, horizontalStackView) in stackArr.enumerated() {
+            for string in btnStrings[verIdx] {
+                let btn = UIButton()
+                btn.setTitle(string, for: .normal)
+                
+                horizontalStackView.addArrangedSubview(btn)
+                btn.setTitleColor(.black, for: .normal)
+                btn.addTarget(self, action: #selector(onClick(_:)), for: .touchUpInside)
+                
+            }
+        }
+        display.textAlignment = .right
+    }
+    
+    @objc func onClick(_ selector: UIButton) {
+        display.text = selector.title(for: .normal)
+        switch display.text {
+        case "x":
+            equationType = .multiply
+        case "-":
+            equationType = .subtract
+        case "+":
+            equationType = .add
+        case "=":
+            equationType = .equals
+        case ".":
+            
+        case "AC":
+            
+        default:
+            
+        }
+    }
+}
